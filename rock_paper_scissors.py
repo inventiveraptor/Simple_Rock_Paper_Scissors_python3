9c635bb4dade1a8da31e81afd7705384fcd26a64
inventiveraptor <68027008+inventiveraptor@users.noreply.github.com>
2021-12-16 09:45:32 -0500

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -27,6 +27,25 @@ while cont == "Y" or cont == "y":
     print("")
     print(f"Computer threw {enemy_throw}!")
 
+    while player_throw == enemy_throw:
+        print("It's a tie! Throw again to settle a winner")
+        print("Rock, Paper, Scissors.......")
+        print("Player, maker your choice ")
+        enemy_throw = random.choice(choices)
+        player_throw = input()
+        time.sleep(2)
+        print("Get ready!")
+        time.sleep(2)
+        print("3...")
+        time.sleep(1)
+        print("2...")
+        time.sleep(1)
+        print("1...")
+        time.sleep(0.5)
+        print("")
+        print("")
+        print(f"Computer threw {enemy_throw}!")
+
     if (player_throw == "Rock" and enemy_throw == "Scissors") or (player_throw == "Paper" and enemy_throw == "Rock") or (player_throw == "Scissors" and enemy_throw == "Paper"):
         print("Player 1 wins!")
         player_score += 1
@@ -44,8 +63,6 @@ print("")
 print(f"High Scores:\nPlayer: {player_score}\nComputer: {enemy_score}")
 
 if enemy_score > player_score:
-    print("You got bitch slapped son!")
+    print("Better luck next time!")
 else: 
-    print("Well done")
-
-
+    print("Well done!")
